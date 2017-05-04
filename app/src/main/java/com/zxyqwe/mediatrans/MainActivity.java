0e54c4d3f2842de074a2e8f2cf75641a5592d502
张小奕 <zxyqwe_2004@126.com>
2017-05-04 22:50:03 +0800

    init

---
+++ app/src/main/java/com/zxyqwe/mediatrans/MainActivity.java
@@ -0,0 +1,71 @@
+package com.zxyqwe.mediatrans;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.zhihu.matisse.Matisse;
+import com.zhihu.matisse.MimeType;
+import com.zhihu.matisse.engine.impl.GlideEngine;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+    private static final int REQUEST_CODE_CHOOSE = 23;
+    Button search_net;
+    Button picker;
+    Button upload;
+    List<Uri> mSelected;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        search_net = (Button) findViewById(R.id.search_net);
+        picker = (Button) findViewById(R.id.picker);
+        upload = (Button) findViewById(R.id.upload);
+        upload.setEnabled(false);
+        search_net.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Matisse.from(MainActivity.this)
+                        .choose(MimeType.allOf())
+                        .countable(true)
+                        .gridExpectedSize(getResources().getDimensionPixelSize(R.dimen.grid_expected_size))
+                        .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+                        .thumbnailScale(0.85f)
+                        .imageEngine(new GlideEngine())
+                        .forResult(REQUEST_CODE_CHOOSE);
+
+            }
+        });
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CHOOSE && resultCode == RESULT_OK) {
+            mSelected = Matisse.obtainResult(data);
+            Log.d(TAG, "mSelected: " + mSelected);
+        }
+    }
+}
