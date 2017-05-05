da29de10e32397c9d64e1d7cb214f15a255e4d35
张小奕 <zxyqwe_2004@126.com>
2017-05-05 10:00:45 +0800

    select

--- app/src/main/java/com/zxyqwe/mediatrans/MainActivity.java
+++ app/src/main/java/com/zxyqwe/mediatrans/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zxyqwe.mediatrans;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
@@ -8,29 +9,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.zhihu.matisse.Matisse;
 import com.zhihu.matisse.MimeType;
-import com.zhihu.matisse.engine.impl.GlideEngine;
+import com.zhihu.matisse.engine.impl.PicassoEngine;
+import com.zhihu.matisse.internal.utils.PhotoMetadataUtils;
 
+import java.io.File;
+import java.text.DecimalFormat;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getName();
+    private static final String TAG = MainActivity.class.getSimpleName();
     private static final int REQUEST_CODE_CHOOSE = 23;
     Button search_net;
     Button picker;
     Button upload;
+    TextView media_stat;
     List<Uri> mSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         search_net = (Button) findViewById(R.id.search_net);
         picker = (Button) findViewById(R.id.picker);
         upload = (Button) findViewById(R.id.upload);
+        media_stat = (TextView) findViewById(R.id.media_stat);
+
         upload.setEnabled(false);
         search_net.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,10 +55,11 @@ public class MainActivity extends AppCompatActivity {
                 Matisse.from(MainActivity.this)
                         .choose(MimeType.allOf())
                         .countable(true)
+                        .maxSelectable(Integer.MAX_VALUE)
                         .gridExpectedSize(getResources().getDimensionPixelSize(R.dimen.grid_expected_size))
                         .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
                         .thumbnailScale(0.85f)
-                        .imageEngine(new GlideEngine())
+                        .imageEngine(new PicassoEngine())
                         .forResult(REQUEST_CODE_CHOOSE);
             }
         });
@@ -68,7 +78,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void renew() {
+        ContentResolver resolver = this.getContentResolver();
+        StringBuilder res = new StringBuilder();
+        String path;
+        File f;
+        for (Uri m :
+                mSelected) {
+            path = PhotoMetadataUtils.getPath(resolver, m);
+            f = new File(path);
+            res.append(path).append(' ').append(exchange(f.length())).append("\r\n");
+        }
+        media_stat.setText(res.toString());
+    }
 
+    private String exchange(long i) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        char[] f = {' ', 'K', 'M', 'G'};
+        double di = (double) i;
+        int j = 0;
+        while (di > 1024) {
+            j++;
+            di /= 1024;
+        }
+        return df.format(di) + f[j] + 'B';
     }
 
     @Override
